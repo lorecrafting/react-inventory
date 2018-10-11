b45c306f5919fe32dec9b8f5291967391593a960
Raymond Luong <raymondluong@Raymonds-MBP.home>
2018-10-10 19:54:41 -1000

    full stack

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { getItemsFromFakeXHR, addItemToFakeXHR, deleteItemByIdFromFakeXHR } from './db/inventory.db';
-
-
-
+import axios from 'axios';
 
 
 class App extends Component {
@@ -24,35 +23,71 @@ class App extends Component {
   }
 
   componentDidMount() {
-    getItemsFromFakeXHR()
+    // getItemsFromFakeXHR()
+    //   .then( items => {
+    //     this.setState({ items })
+    //   }, function() {
+    //     console.log('this.state updated', this.state)
+    //   })
+    axios
+      .get('/items')
       .then( items => {
-        this.setState({ items })
-      }, function() {
-        console.log('this.state updated', this.state)
+        console.log("items", items)
+        this.setState({items: items.data})
+      })
+      .catch( err => {
+        console.log('err', err)
       })
   }
+
+  renderItemList() {
+    if (this.state.hasItems) {
+      return <ItemList items={this.state.items}/>
+    } else {
+      return null
+    }
+  }
+
 
   render() {
 
     console.log('this.state.count', this.state.count)
     return (
+     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>You peek into your inventory:</h1>
+          <Link className="App-title" to="/inventory">Inventory</Link>
+          <Link className="App-title" to="/character">Character</Link>
+          <Link className="App-title" to="/quests/123">Quests</Link>
         </header>
-        <ItemList items={this.state.items}/>
-        <ItemForm addItem={this.addItemToInventory}/>
+        <Route path="/inventory" component={ () => <ItemList items={this.state.items}/>}/>
+        <Route path="/character" component={Character}/>
+        <Route path="/quests/:id" component={Quests}/>
       </div>
+     </Router>
     )
   }
 }
 
 
-function ItemList(props) {
-  return props.items.map( item => <Item key={item.id} name={item.name}/>)
+function Character(props) {
+  return <div>MY FAKE CHARACTER COMPONENT HERE</div>
 }
 
+function Quests(props) {
+  console.log('props', props)
+  return <div>MY FAKE QUEST COMPONENT HERE</div>
+}
+
+function ItemList(props) {
+  
+  return props.items.map( item => <Item key={item.id} name={item.name}/>)
+}
 function Item(props) {
+  console.log('props', props)
+  function navigateTo() {
+    
+  }
   return <div >{props.name}</div>
 }
 
